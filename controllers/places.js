58f508da8b3a91d5fbc31222d1c95f4c9c6b55a5
Ashley Kubizna <113566558+kubizna21@users.noreply.github.com>
2023-03-30 21:22:38 -0700

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -1,26 +1,19 @@
 const router = require('express').Router()
 
-let places = [
-    {
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/thai_food.avif'
-    },
-    {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'https://pixabay.com/photos/coffee-cafe-cat-milk-drink-hot-1711012/'
-    }
-]
-
 
 
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
+
+router.post('/', (req, res) => {
+    req.body.pic = req.body.pic || '/images/default_food.jpg'
+    req.body.city = req.body.city || 'Anytown'
+    req.body.state = req.body.state || 'USA'
+
+    places.push(req.body)
+    res.redirect('/places')
+})
+
 
 module.exports = router
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,14 +1,65 @@
 const React = require('react')
-const Def = require('../default')
+const Default = require('../default')
 
-function new_form () {
+function New() {
     return (
-        <Def>
+        <Default>
             <main>
-                <h1>Add a New Places</h1>
+                <h1>Add a New Place</h1>
+                <br/>
+                <form method='POST' action='/places'>
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input
+                            className="form-control" 
+                            id="name" 
+                            name="name" 
+                            type="text"
+                            required />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="pic">Picture</label>
+                        <input
+                            className="form-control" 
+                            id="pic" 
+                            name="pic" 
+                            type="url" />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="city">City</label>
+                        <input
+                            className="form-control" 
+                            id="city" 
+                            name="city" 
+                            type="text" />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="state">State</label>
+                        <input
+                            className="form-control" 
+                            id="state" 
+                            name="state" 
+                            type="text" />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="cuisines">Cuisines</label>
+                        <input
+                            className="form-control" 
+                            id="cuisines" 
+                            name="cuisines" 
+                            type="text" 
+                            required />
+                    </div>
+                    <br/>
+                    <input className="btn btn-primary" type="submit" value="Add Place" />
+                </form>
             </main>
-        </Def>
+        </Default>
     )
 }
 
-module.exports = new_form
+module.exports = New
