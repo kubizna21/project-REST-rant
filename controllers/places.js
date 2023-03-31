04cc93aecc0135a12f6c6e88be69913788c5fc8a
Ashley Kubizna <113566558+kubizna21@users.noreply.github.com>
2023-03-30 21:10:11 -0700

    Adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -19,8 +19,8 @@ let places = [
 
 
 
-router.get('/', (req, res) => {
-    res.render('places/index', { places })
+router.get('/new', (req, res) => {
+    res.render('places/new')
 })
 
 module.exports = router
